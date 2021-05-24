249714a8aa1155b4fece6a229dd61faa4682b3ca
Brinza Octavian <72095729+TaviBrinza@users.noreply.github.com>
2021-05-24 20:05:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,76 @@
-const progressBar = document.getElementsByClassName('progres-bar')[0]
-setInterval(( )=> {
-    const computedStyle = getComputedStyle(progressBar)
-    const width = parseFloat(computedStyle.getPropertyPriorityValeu('--width'))|| 0
-    progressBar.setProperty('--width',width + .1)
-}, 5)
+let display = document.getElementById('display');
+
+let buttons = Array.from(document.getElementsByClassName('button'));
+
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case 'C':
+                display.innerText = '';
+                break;
+
+             case '←':
+                    if (display.innerText){
+                       display.innerText = display.innerText.slice(0, -1);
+                    }
+                    break;
+            case '=':
+                try{
+                    display.innerText = eval(display.innerText);
+                } catch {
+                    display.innerText = "Error"
+                }
+                break;
+            default:
+                display.innerText += e.target.innerText;
+        }
+    });
+});
+
+const videoElement = document.getElementById('video-input');
+const canvasElement = document.getElementById('canvas-output');
+const canvasCtx = canvasElement.getCotext('2d');
+
+const holistic = new Holistic({locateFile: (file) => {
+    return `https://cdn.jsdelivr.net/npm/@mediapipe/holistic/${file}`;
+  }});
+  holistic.setOptions({
+    modelComplexity: 1,
+    smoothLandmarks: true,
+    minDetectionConfidence: 0.5,
+    minTrackingConfidence: 0.5
+  });
+  function onResults(results) {
+    canvasCtx.save();
+    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+    canvasCtx.drawImage(
+        results.image, 0, 0, canvasElement.width, canvasElement.height);
+    drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS,
+                   {color: '#00FF00', lineWidth: 4});
+    drawLandmarks(canvasCtx, results.poseLandmarks,
+                  {color: '#FF0000', lineWidth: 2});
+    drawConnectors(canvasCtx, results.faceLandmarks, FACEMESH_TESSELATION,
+                   {color: '#C0C0C070', lineWidth: 1});
+    drawConnectors(canvasCtx, results.leftHandLandmarks, HAND_CONNECTIONS,
+                   {color: '#CC0000', lineWidth: 5});
+    drawLandmarks(canvasCtx, results.leftHandLandmarks,
+                  {color: '#00FF00', lineWidth: 2});
+    drawConnectors(canvasCtx, results.rightHandLandmarks, HAND_CONNECTIONS,
+                   {color: '#00CC00', lineWidth: 5});
+    drawLandmarks(canvasCtx, results.rightHandLandmarks,
+                  {color: '#FF0000', lineWidth: 2});
+    canvasCtx.restore();
+  }
+  holistic.onResults(onResults);
+
+  const camera =new Camera(
+      videoElement ,{
+          onFrame : async() => {
+              await holistic.send({image:videoElement});
+          },
+          widt:450,
+          height:450,
+
+      }
+  )
+  camera.start();
